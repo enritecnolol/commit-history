c4c32cf17bd1c72829ae9ab69c2f9f21c54d8c27
Tomas E. Crespo <37670842+enritecnolol@users.noreply.github.com>
2023-08-29 18:44:10 -0400

    add node-fetch dependence

--- apps/backend/src/modules/github/service/github.service.ts
+++ apps/backend/src/modules/github/service/github.service.ts
@@ -1,10 +1,27 @@
 import { Octokit } from '@octokit/rest';
+import fetch from 'node-fetch';
 
 export class GithubService {
   private readonly octokit: Octokit;
 
   constructor() {
-    this.octokit = new Octokit();
+    this.octokit = new Octokit({
+      request: {
+        fetch: fetch,
+      },
+    });
+  }
+
+  reformatCommitsData(commitsData: any[]): any[] {
+    return commitsData.map((commit) => {
+      return {
+        authorName: commit.commit.author.name,
+        authorDate: commit.commit.author.date,
+        message: commit.message,
+        htmlUrl: commit.html_url,
+        avatarUrl: commit.author ? commit.author.avatar_url : null,
+      };
+    });
   }
 
   async getCommits(owner: string, repo: string): Promise<any> {
@@ -13,8 +30,7 @@ export class GithubService {
         owner,
         repo,
       });
-
-      return response.data;
+      return this.reformatCommitsData(response.data);
     } catch (error) {
       throw new Error(`Failed to fetch commit: ${error.message}`);
     }
