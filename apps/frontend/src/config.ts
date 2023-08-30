4ec523be5061d7bfeee3c512a76ed21f9cd588df
Tomas E. Crespo <37670842+enritecnolol@users.noreply.github.com>
2023-08-29 22:29:06 -0400

    create config file for handle ENV var

---
+++ apps/frontend/src/config.ts
@@ -0,0 +1,3 @@
+export const API_URL = import.meta.env.VITE_API_URL;
+export const GITHUB_OWNER = import.meta.env.VITE_GITHUB_OWNER;
+export const GITHUB_REPOSITORY = import.meta.env.VITE_GITHUB_REPO;
--- apps/frontend/src/services/index.ts
+++ apps/frontend/src/services/index.ts
@@ -1,23 +1,21 @@
 import { Commit } from "../types/types";
-
-const baseUrl = import.meta.env.VITE_API_URL;
-const Owner = import.meta.env.VITE_GITHUB_OWNER
-const Repo = import.meta.env.VITE_GITHUB_REPO
+import { API_URL, GITHUB_OWNER, GITHUB_REPOSITORY } from "../config";
 
 const getCommits = async (): Promise<Commit[]> => {
-    try {
-        const response = await fetch(`${baseUrl}/github/commits?` + new URLSearchParams({
-            owner: Owner,
-            repo: Repo
-        }));
-        const { data } = await response.json();
-        return data;
-    } catch (error) {
-        console.error('An error occurred:', error);
-        throw error;
-    }
-}
+  try {
+    const response = await fetch(
+      `${API_URL}/github/commits?` +
+        new URLSearchParams({
+          owner: GITHUB_OWNER,
+          repo: GITHUB_REPOSITORY,
+        })
+    );
+    const { data } = await response.json();
+    return data;
+  } catch (error) {
+    console.error("An error occurred:", error);
+    throw error;
+  }
+};
 
-export {
-    getCommits
-}
+export { getCommits };
