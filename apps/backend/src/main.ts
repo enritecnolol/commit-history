15b7ec5646a2a9127724aa12096e2231be1e2c37
Tomas E. Crespo <37670842+enritecnolol@users.noreply.github.com>
2023-08-29 22:29:52 -0400

    add http filter for error handling

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformationInterceptor } from './interceptors/transform.interceptor';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new TransformationInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('/api');
   app.enableCors();
   await app.listen(3000);
