faf4758f3363940e5dfe5932a8ef20a4531413c1
Tomas E. Crespo <37670842+enritecnolol@users.noreply.github.com>
2023-08-29 21:42:21 -0400

    add commit history page with request

---
+++ apps/frontend/src/pages/CommitHistoryPage.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import CommitItem from "../components/CommitItem";
+import { Commit } from "../types/types";
+import { getCommits } from "../services";
+
+export default function CommitHistoryPage() {
+
+  const [commits, setCommits] = useState<Commit[]>([])
+
+  useEffect(() => {
+    getCommits().then((response) => {
+      setCommits(response)
+    })
+  }, [])
+
+  return (
+    <div className="w-full flex justify-center">
+      <div className="flex flex-col gap-y-5 mb-4">
+        <div className="text-2xl pb-2 border-b-2 border-slate-800">Commits</div>
+        {commits.map((_, index) => {
+          return <CommitItem key={index} />;
+        })}
+      </div>
+    </div>
+  );
+}
